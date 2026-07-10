5e2ffad7bda9bc5527a8b3d526df0524c15fda38
mahdinejati0131-dot <mahdinejati0131@gmail.com>
2026-07-11 03:29:15 +0330

    bot.py
    import telebot
    
    TOKEN = '8800613146:AAEaDznhFmIiOM_iJBAdOkqCBDlsV8r96Iw'
    bot = telebot.TeleBot(TOKEN)
    
    # آیدی فایل شما
    TECH_FILE_ID = 'AgADbQsAArcL2VE'
    
    @bot.message_handler(commands=['start'])
    def start(message):
        bot.reply_to(message, "سلام! نام کتاب را بنویس.")
    
    @bot.message_handler(func=lambda message: message.text == "تکنولوژی آموزشی")
    def send_tech(message):
        bot.send_document(message.chat.id, TECH_FILE_ID)
    
    bot.polling()

---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+
+TOKEN = '8800613146:AAEaDznhFmIiOM_iJBAdOkqCBDlsV8r96Iw'
+bot = telebot.TeleBot(TOKEN)
+
+# آیدی فایل شما
+TECH_FILE_ID = 'AgADbQsAArcL2VE'
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "سلام! نام کتاب را بنویس.")
+
+@bot.message_handler(func=lambda message: message.text == "تکنولوژی آموزشی")
+def send_tech(message):
+    bot.send_document(message.chat.id, TECH_FILE_ID)
+
+bot.polling()
